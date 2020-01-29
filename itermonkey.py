edd6a2354008b478f2a9411844118de8f69c0fad
Lucio Paiva <luciopaiva@gmail.com>
2020-01-29 01:55:25 +0000

    First Applescript experiment

--- itermonkey.py
+++ itermonkey.py
@@ -4,6 +4,7 @@ import sys
 import getopt
 import argparse
 import re
+import subprocess
 from os.path import expanduser
 
 home = expanduser("~")
@@ -19,6 +20,7 @@ parser = argparse.ArgumentParser(
 parser.add_argument("-r", "--run", dest="should_actually_run", action="store_true", default=False, 
     help="Must pass this to actually run, otherwise will just list matching hosts")
 parser.add_argument("pattern", help="A regular expression used to select hosts by name")
+
 
 def load_hosts():
     with open(home + "/.ssh/config") as f:
@@ -44,15 +46,72 @@ def do_safety_check(hosts_count):
     return True
 
 
+def check_if_iterm_version_is_supported():
+    osa = subprocess.Popen(['osascript', '-'],
+                            stdin=subprocess.PIPE,
+                            stdout=subprocess.PIPE)
+
+    version_script = 'set iterm_version to (get version of application "iTerm")'
+    version = osa.communicate(version_script)[0].strip()
+
+    match = re.search(r"^(\d+)\.(\d+)", version)
+    if match:
+        major = int(match.group(1))
+        minor = int(match.group(2))
+        return (major > 2) or (major == 2 and minor > 9)  # support only if greater than 2.9
+    return False
+
+
+def prompt_for_confirmation():
+    sys.stdout.write("Press 'y' to continue or anything else to abort: ")
+    choice = raw_input().strip().lower()
+    return choice == "y"
+
+
+def prepare_and_run_applescript(selected_hosts):
+    osa = subprocess.Popen(['osascript', '-'],
+                            stdin=subprocess.PIPE,
+                            stdout=subprocess.PIPE)
+
+    script_lines = """
+        tell application "iTerm"
+            activate
+            tell current window
+                create tab with default profile
+            end tell
+
+            set pane_1 to (current session of current window)
+
+            tell pane_1
+                write text "ssh {host}"
+                set name to "this is me testing"
+            end tell
+
+        end tell
+        """.format(host=selected_hosts[0])
+    script = script_lines  # "\n".join(script_lines)
+    output = osa.communicate(script)[0]
+    print(output)
+
+
 def main():
+    if not check_if_iterm_version_is_supported():
+        print("iTerm2 version not supported or iTerm2 is not installed")
+        exit(1)
+
     arguments = parser.parse_args()
     pat = re.compile(arguments.pattern, re.IGNORECASE)
     load_hosts()
 
     # get filtered list of hosts, each a tuple (name, address)
     selected_hosts = [(name, address) for (name, address) in hostname_by_host.items() if pat.search(name)]
+    print("Will open the following terminal panes:")
     if do_safety_check(len(selected_hosts)):
         for (name, address) in sorted(selected_hosts, key=lambda tuple: tuple[0]):  # sort by host name
-            print("%s: %s" % (name, address))
+            print("- %s (%s)" % (name, address))
+    
+    if prompt_for_confirmation():
+        prepare_and_run_applescript([name for (name, address) in selected_hosts])
+
 
 main()
